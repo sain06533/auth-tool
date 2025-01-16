41ee14aa0de8cdcf8eaf4b99b5721e84f64b49b2
sain06533 <sain06533@gmail.com>
2025-01-16 11:37:35 +0530

    package update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const multer = require('multer');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
